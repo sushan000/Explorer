d5029e14e54f81bb8660e9a6a39e8064e118ef7d
shan <31597683+sushan000@users.noreply.github.com>
2017-10-15 20:34:09 -0500

    test commit

---
+++ src/com/explorer/panel/ExplorerLeft.java
@@ -0,0 +1,17 @@
+package com.explorer.panel;
+
+import javax.swing.JPanel;
+
+public class ExplorerLeft extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public ExplorerLeft() {
+		System.out.println("test display");
+	}
+	public void testPush() {
+		System.out.println("test push");
+	}
+
+}
---
+++ src/com/explorer/panel/ExplorerRight.java
@@ -0,0 +1,14 @@
+package com.explorer.panel;
+
+import javax.swing.JPanel;
+
+public class ExplorerRight extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public ExplorerRight() {
+
+	}
+
+}
